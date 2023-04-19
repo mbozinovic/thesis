ad252639ac89ac2f44760a2e8ce1dbe059fdf304
mbozinovic <116682254+mbozinovic@users.noreply.github.com>
2023-04-19 13:18:20 -0700

    Created separate scripts

---
+++ data/soundscape_metrics.R
@@ -0,0 +1,31 @@
+## code to prepare `soundscape` dataset goes here
+
+#NEED TO MAKE THIS A FUNCTION
+# look at http://ohi-science.org/data-science-training/programming.html#automation-with-for-loops for help on automation.
+
+library(tidyverse)
+library(lubridate)
+
+
+#dlist <- c(4,7,8,10,12,13,14,16,17,18,19,20,21,22,23)
+sslist <- list.files(path = paste0("data-raw/"), pattern = "CCES_", recursive = TRUE)
+
+#code doesnt work, can delete
+#for (i in sslist) {
+#  read_csv(paste0("data-raw/",i), show_col_types = FALSE)%>%
+#  rename(dateTime = `yyyy-mm-ddTHH:MM:SSZ`) %>%
+#  mutate(dateTime = round_date(ymd_hms(.$dateTime),"20 minutes")) %>%
+#  assign(., value = paste0((substr(sslist[i], 16, 17)), substr(sslist[i], 6, 7)))
+#}
+
+#From ChatGPT, edited from above
+for (i in sslist) {
+  data <- read_csv(here(paste0("data-raw/", i)), show_col_types = FALSE) %>%
+    rename(dateTime = `yyyy-mm-ddTHH:MM:SSZ`) %>%
+    mutate(dateTime = round_date(ymd_hms(dateTime), "20 minutes")) %>%
+    mutate(variable = paste0(substr(i, 16, 17), substr(i, 6, 7)))
+  
+  assign(paste0(substr(i, 16, 17), "_", substr(i, 6, 7)), data)
+}
+
+# Need to save as .rda
---
+++ data/whales.R
@@ -0,0 +1,25 @@
+## code to prepare `whale` dataset here
+
+library(tidyverse)
+
+# Load raw RDA file
+#load("data-raw/CCES2018_BW_Detections.rda")             # Beaked whales only
+load("data-raw/CCES2018_BW_and_PM_Detections.rda")       # Beaked  + sperm whales
+
+# Change EventInfo object name to "whales" and edit columns
+whales <- EventInfo %>%
+  dplyr::select(-Project, -UID, -Id) %>%            # remove unnecessary columns
+  subset(species!= "?BW") %>%                    # remove rows containing ?BW and BW
+  subset(species!= "BW") %>%
+  mutate(dateTimeRound = round_date(.$StartTime, "20 minutes")) %>%   # Round to nearest 20 min
+  rename(station = Deployment)                    # Rename for common join column name next
+
+## NEED TO CHANGE STATION 15 TO STATION 14 HERE. Drift 15 and 14 are the same.
+whales$station[whales$station == 15] <- 14
+
+# Create common UTC field and remove unnecessary fields
+whales <- left_join(whales, tracks, by = join_by(station, closest("dateTimeRound" <= UTC))) %>%
+  dplyr::select(-Latitude.y, -Longitude.y, -spotID, -minNumber, -maxNumber, -bestNumber)
+
+# Whales data as sf
+BWsf <- whales %>% st_as_sf(coords = c("Longitude.x","Latitude.x"), crs=4326)   # whales as sf
