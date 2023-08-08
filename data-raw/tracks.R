bf8c23379e0a250be7dac16ec11ac1d2fed29ef9
mbozinovic <116682254+mbozinovic@users.noreply.github.com>
2023-08-07 17:20:08 -0700

    Update tracks.R
    added track4 to see alltracks without rounding

--- data-raw/tracks.R
+++ data-raw/tracks.R
@@ -8,9 +8,9 @@ load("data-raw/CCES2018_DriftTracks_Modified_03Nov2022.rda")
 # Read in tracks, filter out lost buoys (1,2,3,5,6,9,11) and corrupted buoys (4,17)
 tracks <- AllTracks %>%
   dplyr::select(-dist, -speed) %>%
-  mutate(dateTime = round_date(.$dateTime, "20 minutes")) %>%      # round to nearest 20 minutes, will create duplicated in the data
+  mutate(dateTime = round_date(.$dateTime, "20 minutes")) %>% # round to nearest 20 minutes, will create duplicated in the data
   filter(!station %in% c('1','2','3','4','5','6','9','11', '17')) %>%
-  rename(Longitude = long,                                 # Need these columns to specifically say this for ERDDAP matching
+  rename(Longitude = long,              # Need these columns to specifically say this for ERDDAP matching
          Latitude = lat,
          UTC = dateTime)
 tracks <- tracks[!duplicated(tracks[c('UTC', 'station')]),]    # Remove duplicates
@@ -21,3 +21,16 @@ saveRDS(tracks, 'data/tracks.rda')
 # Filter by buoy
 #tracks_08 <- tracks %>% dplyr::filter(station == "8") 
 track4 <- AllTracks %>% dplyr::filter(station == "4")
+
+
+
+### Option to create AllTracks before rounding to nearest 20 min, for AIS
+tracks2 <- AllTracks %>%
+  dplyr::select(-dist, -speed) %>%
+  filter(!station %in% c('1','2','3','4','5','6','9','11', '17')) %>%
+  rename(Longitude = long,            # Need these columns to specifically say this for ERDDAP matching
+         Latitude = lat,
+         UTC = dateTime)
+
+tracks7 <- tracks2 %>%
+  filter(station== 7)
