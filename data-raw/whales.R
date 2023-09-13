55b005c9338127c1863503c73d7528d587dffafa
mbozinovic <116682254+mbozinovic@users.noreply.github.com>
2023-09-13 09:51:41 -0700

    removed rounding, prepped for join

--- data-raw/whales.R
+++ data-raw/whales.R
@@ -1,32 +1,23 @@
 ## code to prepare `whales` dataset here
 
+# Required library
 library(tidyverse)
-library(sf)
 
 # Load raw RDA file
-#load("data-raw/CCES2018_BW_Detections.rda")             # Beaked whales only
-load("data-raw/CCES2018_BW_and_PM_Detections.rda")       # Beaked + sperm whales
+load("data-raw/CCES2018_BW_and_PM_Detections.rda")
 
 # Change EventInfo object name to "whales" and edit columns
 whales <- EventInfo %>%
-  dplyr::select(-Project, -UID, -Id) %>%            # remove unnecessary columns
-  subset(species!= "?BW") %>%                    # remove rows containing ?BW and BW
-  subset(species!= "BW") %>%
-  mutate(dateTimeRound = round_date(.$StartTime, "20 minutes")) %>%   # Round to nearest 20 min
-  rename(station = Deployment)                    # Rename for common join column name next
+  dplyr::select(-Project, -UID, -Id, -minNumber, -maxNumber, -bestNumber, -nClicks) %>%   # remove unnecessary columns
+  subset(species!="?BW" & species!="BW") %>%        # remove rows containing ?BW and BW
+  mutate(UTC = StartTime) %>%
+  filter(!Deployment %in% c('1','2','3','4','5','6','9','11', '17'))
 
-## NEED TO CHANGE STATION 15 TO STATION 14 HERE. Drift 15 and 14 are the same.
-whales$station[whales$station == 15] <- 14
-
-# Create common UTC field and remove unnecessary fields
-whales <- left_join(whales, tracks, by = join_by(station, closest("dateTimeRound" <= UTC))) %>%
-  dplyr::select(-Latitude.y, -Longitude.y, -spotID, -minNumber, -maxNumber, -bestNumber)
-
-# Whales data as sf
-Wsf <- whales %>% st_as_sf(coords = c("Longitude.x","Latitude.x"), crs=4326)   # whales as sf
+## Change Station 15 to Station 14 here. Drift 15 and 14 are the same (mix-up during fieldwork)
+whales$Deployment[whales$Deployment == 15] <- 14
 
 # Save object
 saveRDS(whales, 'data/whales.rda')
 
-# Number of rows per station
-nrow(whales[whales$station==12,])
+
+
