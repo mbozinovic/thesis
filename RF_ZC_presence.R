bec65b34d7c387f35c79f28b85f3e39addf58995
mbozinovic <116682254+mbozinovic@users.noreply.github.com>
2023-11-21 13:54:05 -0800

    Update RF_ZC_presence.R

--- RF_ZC_presence.R
+++ RF_ZC_presence.R
@@ -10,17 +10,19 @@ library(rfPermute)
 #Define response variable (presence/absence as 0/1)
 #I suggest creating a variable for presence of each species, and then create a
 #random forest model for each species 
+load("data/allDrifts.rda")
+allDrifts$newrow <- sample(10000, size = nrow(allDrifts), replace = TRUE)
 
 # For ZC only
-allDrifts$BWpresence<-as.factor(allDrifts$BWpresence) %>%
-  droplevels(c("BB", "BW43", "BW37V", "PM", "BWC"))
+allDrifts$Wpresence<-as.factor(allDrifts$Wpresence) %>%
+  droplevels(c("BB", "BW43", "PM"))
 
 
 #List all variables you want to evaluate (I only included a few here, 
 #but I know you have more environmental and AIS parameters to add)
-covariate.list<-list(c('TOL_125','TOL_2000','TOL_5000',
+covariate.list<-list(c('TOL_125','TOL_2000',
                        'sst_mean','dist2slope','depth',
-                       'chlorophyll_mean','mldDepth'))
+                       'ssh_mean', 'bathy_slope','newrow'))
 
 include.covars <- which(names(allDrifts) %in% covariate.list[[1]])
 
@@ -31,12 +33,12 @@ string.covars.used <- paste0(names(allDrifts)[include.covars], sep="+", collapse
 #DF.modelZC <- na.omit(allDrifts)
 
 DF.modelZC <- allDrifts %>%
-  filter(!species %in% c("BB", "BW43", "BW37V", "PM", "BWC"))
+  filter(!species %in% c("BB", "BW43", "PM"))
 
 # create balanced sample sizes of response for tree construction to 
 # avoid biases associated with imbalanced data
 # For ZC only
-sampsizeZC <- balancedSampsize(DF.modelZC$BWpresence)
+sampsizeZC <- balancedSampsize(DF.modelZC$Wpresence)
 
 # set seed for reproducibility
 set.seed(123)
@@ -44,7 +46,7 @@ set.seed(123)
 # create balanced sample size random forest model
 
 #Model for ZC
-RF.model.ZC <- rfPermute(DF.modelZC$BWpresence ~ ., DF.modelZC[,include.covars], ntree=1000) ##doesnt work!!
+RF.model.ZC <- rfPermute(DF.modelZC$Wpresence ~ ., DF.modelZC[,include.covars], ntree=100) ##doesnt work!!
 RF.model.ZC
 
 Dep <- "CCES_ZC_"
@@ -55,7 +57,7 @@ pdf(paste(Dep,"_",string.covars.used,".pdf"), width=14, height=10) ## Not workin
 round(importance(RF.model.ZC),3) #ranking of importance for each variable
 plotImportance(RF.model.ZC) #visualize importance
 plotTrace(RF.model.ZC)   #model stability w/number of trees (this should be flat!)
-plotImpPreds(RF.model.ZC, DF.model, "Bwpresence")  #distribution of predictors 
+plotImpPreds(RF.model.ZC, DF.model, "Wpresence")  #distribution of predictors 
 plotPredictedProbs(RF.model.ZC)
 plotProximity(RF.model.ZC) ## Not working?? 
 dev.off()
